c17faf4bc95de141200006d1d50c45b4ec697fd2
Alan Wang <alanwang53@gmail.com>
2021-03-19 23:08:08 -0400

    modified Icons

--- examples/mobile-product-mfe/src/component/Icon.js
+++ examples/mobile-product-mfe/src/component/Icon.js
@@ -1,13 +1,12 @@
-
 const Icon = ({ key, value }) => {
-    
+	<h3>{key}</h3>;
 
-    return (
-        console.log(key),
-        <div className='icon'>
-            <h3>{value}</h3>
-        </div>
-    )
-}
+	return (
+		// <div className='icon'>
+		// 	<h3>{value}</h3>
+		// </div>
+		<div></div>
+	);
+};
 
-export default Icon
+export default Icon;
--- examples/mobile-product-mfe/src/component/Icons.js
+++ examples/mobile-product-mfe/src/component/Icons.js
@@ -1,40 +1,19 @@
+import React from "react";
 import Icon from "./Icon";
 
 const Icons = ({ icons }) => {
 	console.log(icons);
 
-	// for (const key in icons) {
-	// 	// console.log(`${key}: ${icons[key]}`);
+	let elem = Object.keys(icons);
+	let elem2 = Object.values(icons);
+	console.log(elem);
+	console.log(elem2);
 
-	// 	return <Icon value={icons[key]}></Icon>;
-	// }
-	let task = [
-		{
-			key: 1,
-			text: "1 Button",
-			reminder: true,
-		},
-		{
-			key: 2,
-			text: "2 Button",
-			reminder: false,
-		},
-	];
+	for (const [key, value] of Object.entries(icons)) {
+		console.log(`${key} ${value}`);
+	}
 
-	// console.log(task[0].key);
-
-	return (
-		// Using a map function to render each icon
-		<div className='icons'>
-			{/* {task.map((s) => {
-				<Icon key={s.key} value={s.value}></Icon>;
-			})} */}
-
-			{task.map((s, index) => {
-				<Icon key={index} value={s}></Icon>;
-			})}
-		</div>
-	);
+	return <div className='icons'></div>;
 };
 
 export default Icons;
