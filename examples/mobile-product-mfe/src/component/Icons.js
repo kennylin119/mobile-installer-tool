e2fab8ab5aa23ab0514aae17eab0f7194e2a4a4d
Alan Wang <alanwang53@gmail.com>
2021-03-22 20:21:47 -0400

    working state

--- examples/mobile-product-mfe/src/component/Icons.js
+++ examples/mobile-product-mfe/src/component/Icons.js
@@ -9,41 +9,15 @@ const Icons = (props) => {
 	
 	// Object.keys() method returns an array of a given object's own enumerable property names, iterated in the same order that a normal loop would
 	const v = Object.keys(icons)
-	v.map(function(key, index) {
-		// console.log(key)
-		// console.log(icons[key])
-	})
-	
-
-	// for (key in v) {
-	// 	<Icon icon_key={key} icon_value={icons[key]}/>
-	// }
-
-	const arr = ['key1','key2','key3']
 
 	return (
-		<ul>
-
-			{/* <Icon icon_key={v[5]} icon_value={icons[v[5]]}/>
-
-			for (let {i = 0; i < v.length; i++) {
-				{ <Icon icon_key={v[i]} icon_value={icons[v[i]]}/> }
-			} */}
-
+		<div>
 			{
-				v.forEach(key => {
-					// console.log(key)
-					// console.log(icons[key])
-					return <Icon icon_key={key} icon_value={icons[key]}></Icon>
-					// <li>{key}</li>
-				})
+				v.map(key => (
+					<Icon icon_key={key} icon_value={icons[key]}></Icon>
+				))
 			}
-
-			{/* { arr.map(function(arrKey) {	
-				<p>{arrKey}</p>		
-				// <Icon icon_key={key} icon_value={icons[key]}/>
-			})}  */}
-		</ul>
+		</div>
 
 	)
 };
