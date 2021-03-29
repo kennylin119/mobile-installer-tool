7fdc56480a496b19c84573d246b5ea8d319b6588
Hannah Fabian <hmf217@lehigh.edu>
2021-03-29 19:26:58 -0400

    Code review

--- examples/mobile-product-mfe/src/component/Dropdowns/Dropdown.js
+++ examples/mobile-product-mfe/src/component/Dropdowns/Dropdown.js
@@ -1,13 +1,33 @@
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import default_img from '../../toolkit/default_image.jpeg';
 
-const Dropdown = () => {
-    return (
-        <DropdownButton id="dropdown-item-button" title="Dropdown button">
-            <Dropdown.Item as="button">Action</Dropdown.Item>
-            <Dropdown.Item as="button">Another action</Dropdown.Item>
-            <Dropdown.Item as="button">Something else</Dropdown.Item>
-        </DropdownButton>
-    )
+const handleOnClick = (event, key, value) => {
+	event.preventDefault();
 }
 
-export default Dropdown
+const Dropdown = (props) => {
+
+    // Using default images, as seen in Icon.js logic
+	const {icon_key, cdn, icon_value, icon_image} = props
+	let image; 
+
+	// Handle if there is an image or not
+	if(cdn && icon_image) {
+		image = `${cdn}${icon_image}`
+	}
+	else {
+		image = default_img
+	}
+
+	return (
+        <DropdownButton id="dropdown-item-button" title="Dropdown button">
+            <Dropdown.Item as="button">
+                <img id="icon" src={image} alt={"Image invalid"} onClick={((e) => handleOnClick(e, icon_key, icon_value))}></img>
+                Here is custom option!
+            </Dropdown.Item>
+        </DropdownButton>
+	);
+};
+
+export default Dropdown;
+
