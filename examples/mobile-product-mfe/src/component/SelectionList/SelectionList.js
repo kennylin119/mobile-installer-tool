f3224be4032416eec2726f2ffd4ad2de1d740416
Kenny Lin <kennylin119>
2021-04-19 12:07:03 -0400

    added qol change to the selectionlist

--- examples/mobile-product-mfe/src/component/SelectionList/SelectionList.js
+++ examples/mobile-product-mfe/src/component/SelectionList/SelectionList.js
@@ -1,41 +1,55 @@
-import React, { useState, useContext } from "react"
+import React, { useState, useContext } from "react";
 
 // import context api
-import { Product } from "../../product-context"
-import { ZoomRequest, ZoomResponse } from "../../zoom-context"
-import ZoomHandler from "../../services/ZoomHandler"
+import { Product } from "../../product-context";
+import { ZoomRequest, ZoomResponse } from "../../zoom-context";
+import ZoomHandler from "../../services/ZoomHandler";
 
-import SelectionButton from "./SelectionButton"
+import SelectionButton from "./SelectionButton";
 
 const SelectionList = (props) => {
-	console.log("[SelectionList]")
-	const { variable, data, cdn } = props
+  console.log("[SelectionList]");
+  const { variable, data, cdn } = props;
 
-	console.log("[printing User Controls")
-	console.log(data)
+  console.log("[printing User Controls");
+  console.log(data);
 
-	// console.log("[Printing Matched Components");
-	// console.log(selectionLists);
+  // console.log("[Printing Matched Components");
+  // console.log(selectionLists);
 
-	// console.log("[Printing Option Values]");
-	// console.log(selectionLists[0].OptionValues);
+  // console.log("[Printing Option Values]");
+  // console.log(selectionLists[0].OptionValues);
 
-	const { zoomReqVal, setZoomReq } = useContext(ZoomRequest)
-	const { zoomResVal, setZoomRes } = useContext(ZoomResponse)
+  const { zoomReqVal, setZoomReq } = useContext(ZoomRequest);
+  const { zoomResVal, setZoomRes } = useContext(ZoomResponse);
 
-	// console.log("[printing zoomReqVal");
-	// console.log(zoomReqVal);
+  // console.log("[printing zoomReqVal");
+  // console.log(zoomReqVal);
 
-	// console.log("[printing zoomResVal");
-	// console.log(zoomResVal);
-	return (
-		<div>
-			<h3>{data.Variable}</h3>
-			{data.OptionValues.map((option) => {
-				return <SelectionButton label={option.Label} />
-			})}
-		</div>
-	)
-}
+  // console.log("[printing zoomResVal");
+  // console.log(zoomResVal);
 
-export default SelectionList
+  let output;
+  if (data.OptionValues.length > 7) {
+    output = (
+      <p>
+        {" "}
+        This component is {data.OptionValues.length} length so it was not
+        rendered. It will be a dropdown in the future {" "}
+      </p>
+    );
+  } else {
+    output = data.OptionValues.map((option) => {
+      return <SelectionButton label={option.Label} />;
+    });
+  }
+
+  return (
+    <div>
+      <h3>{data.Variable}</h3>
+      {output}
+    </div>
+  );
+};
+
+export default SelectionList;
