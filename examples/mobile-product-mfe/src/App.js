fe208ce64337ce0e525897ac77085da927098c9e
Alan Wang <alanwang53@gmail.com>
2021-04-22 14:44:20 -0400

    added application footer

--- examples/mobile-product-mfe/src/App.js
+++ examples/mobile-product-mfe/src/App.js
@@ -1,41 +1,52 @@
-import "./App.css";
+import "./App.css"
 
-import React, { useState, useEffect, useContext } from "react";
-// import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from "react-html-parser"
+import React, { useState, useEffect, useContext } from "react"
+import styled from "styled-components"
 
 // importing the context API
-import { Product } from "./product-context";
-import { ZoomRequest, ZoomResponse } from "./zoom-context";
+import { Product } from "./product-context"
+import { ZoomRequest, ZoomResponse } from "./zoom-context"
 
 // importing all the components
-import Icons from "./component/Icons/Icons";
-import NativeDropdown from "./component/SelectionList/NativeDropdown";
-import ProductImage from "./component/ProductImage/ProductImage";
-import { TestComponent } from "./component/Test-Component/TestComponent";
-import { router, fetchUITemplate, fetchDPM, fetchImage } from "./router/router"; // Note: fetchProduct is a singleton
+import Icons from "./component/Icons/Icons"
+import NativeDropdown from "./component/SelectionList/NativeDropdown"
+import ProductImage from "./component/ProductImage/ProductImage"
+import { TestComponent } from "./component/Test-Component/TestComponent"
+import { router, fetchUITemplate, fetchDPM, fetchImage } from "./router/router" // Note: fetchProduct is a singleton
 // importing routing functions
-import ZoomHandler from "./services/ZoomHandler";
-import SelectionList from "./component/SelectionList/SelectionList.js";
+import ZoomHandler from "./services/ZoomHandler"
+import SelectionList from "./component/SelectionList/SelectionList.js"
 
 // Maps Lutron componentTypes to our componentTypes
 // ! IF YOU CHANGE THE NAME OF YOUR COMPONENT, CHANGE THE NAME ON THE RIGHT HERE
 const componentMapper = {
-  SelectionList: "SelectionList",
-//   ImageSelectionList: "NativeDropdown",
-  ImageSelectionList: "Icons",
-  ImageSelector: "Icons",
-  ProductImage: "ProductImage",
-  // Engraving: "Engraving",
-  MetalColor: "SelectionList",
-  // Number: "Double",			// Figure out what Number should be
-  Double: "Value",
-  String: "Value",
-  int: "Value",
-  // Fabric: "Fabric",			// Fabric should be a grouped component that imports SelectionSlider and Icons
-  // Grouped: "Grouped",			// TBD
-  // HorizontalLine: "HorizontalLine",
-  // RowGroup: "RowGroup",
-};
+	SelectionList: "SelectionList",
+	//   ImageSelectionList: "NativeDropdown",
+	ImageSelectionList: "Icons",
+	ImageSelector: "Icons",
+	ProductImage: "ProductImage",
+	// Engraving: "Engraving",
+	MetalColor: "SelectionList",
+	// Number: "Double",			// Figure out what Number should be
+	Double: "Value",
+	String: "Value",
+	int: "Value",
+	// Fabric: "Fabric",			// Fabric should be a grouped component that imports SelectionSlider and Icons
+	// Grouped: "Grouped",			// TBD
+	// HorizontalLine: "HorizontalLine",
+	// RowGroup: "RowGroup",
+}
+
+const Application = styled.div`
+	left: 0;
+	top: 0;
+	right: 0;
+	bottom: 0;
+`
+
+const ApplicationFooter = styled.div`
+	padding: 10px;
+`
 
 /**
  * This function takes in product and configure and parses both objects to build out
@@ -47,157 +58,157 @@ const componentMapper = {
  * @returns == zoom object
  */
 const initializeZoom = (uitemplate, dpm, configure) => {
-  if (uitemplate && dpm) {
-    console.log("[building initial zoom with uitemplate and DPM]");
-    console.log(uitemplate);
-    console.log(dpm);
+	if (uitemplate && dpm) {
+		console.log("[building initial zoom with uitemplate and DPM]")
+		console.log(uitemplate)
+		console.log(dpm)
 
-    if (configure) {
-      configure = JSON.parse(configure);
+		if (configure) {
+			configure = JSON.parse(configure)
 
-      console.log("[and user configurations]");
-      console.log(configure);
-    }
+			console.log("[and user configurations]")
+			console.log(configure)
+		}
 
-    // Using optional chaining ?.
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
-    // TODO: familiarize with what each of these attributes mean
-    // ! Not every zoom variable has all of these attributes. Are there other attributes that we missed?
+		// Using optional chaining ?.
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+		// TODO: familiarize with what each of these attributes mean
+		// ! Not every zoom variable has all of these attributes. Are there other attributes that we missed?
 
-    let _warnings = [];
-    let _features = {};
-    let _additionalAttributes = [];
-    let _featureDependencies = {};
+		let _warnings = []
+		let _features = {}
+		let _additionalAttributes = []
+		let _featureDependencies = {}
 
-    if (uitemplate?.UserControls) {
-      uitemplate.UserControls.map((obj) => {
-        let key = obj?.Variable;
-        let config_key = configure?.Selections[key]; // configure.Selections.SYSTEM => HW
-        // let config_label = configure.SelectionValues["SYSTEM"]
-        let obj_OptionValues = obj?.OptionValues;
+		if (uitemplate?.UserControls) {
+			uitemplate.UserControls.map((obj) => {
+				let key = obj?.Variable
+				let config_key = configure?.Selections[key] // configure.Selections.SYSTEM => HW
+				// let config_label = configure.SelectionValues["SYSTEM"]
+				let obj_OptionValues = obj?.OptionValues
 
-        let _validKeys = [];
+				let _validKeys = []
 
-        // obj_OptionValues.map((option) => {
-        // 	let _keyValue = option?.KeyValue
+				// obj_OptionValues.map((option) => {
+				// 	let _keyValue = option?.KeyValue
 
-        // 	if (_keyValue) {
-        // 		_validKeys.push(_keyValue)
-        // 	}
-        // })
+				// 	if (_keyValue) {
+				// 		_validKeys.push(_keyValue)
+				// 	}
+				// })
 
-        // Setting object at the key
-        // _features[key] = {
-        // 	ValidKeys: _validKeys,
-        // 	InvalidKeys: [],
-        // 	CurrentValue: config_key,
-        // }
+				// Setting object at the key
+				// _features[key] = {
+				// 	ValidKeys: _validKeys,
+				// 	InvalidKeys: [],
+				// 	CurrentValue: config_key,
+				// }
 
-        // Handle the feature dependencies if there are any
-        let tempObj = dpm.Features[key];
+				// Handle the feature dependencies if there are any
+				let tempObj = dpm.Features[key]
 
-        if (tempObj) {
-          let dependantFeatures = tempObj?.DependentFeatures;
+				if (tempObj) {
+					let dependantFeatures = tempObj?.DependentFeatures
 
-          // If a dependant feature exists
-          if (dependantFeatures && dependantFeatures.length > 0) {
-            // Map over each dependantFeature
-            {
-              dependantFeatures.map((val) => {
-                _featureDependencies[val] = [key];
-              });
-            }
-          }
-        }
-      });
-    }
+					// If a dependant feature exists
+					if (dependantFeatures && dependantFeatures.length > 0) {
+						// Map over each dependantFeature
+						{
+							dependantFeatures.map((val) => {
+								_featureDependencies[val] = [key]
+							})
+						}
+					}
+				}
+			})
+		}
 
-    // console.log("[feature dependencies]")
-    // console.log(_featureDependencies)
+		// console.log("[feature dependencies]")
+		// console.log(_featureDependencies)
 
-    // if (configure?.Warnings) {
-    // 	_warnings = configure?.Warnings
-    // }
+		// if (configure?.Warnings) {
+		// 	_warnings = configure?.Warnings
+		// }
 
-    // if (configure?.ResultantValue) {
-    // 	_additionalAttributes.push(configure?.ResultantValue)
-    // }
+		// if (configure?.ResultantValue) {
+		// 	_additionalAttributes.push(configure?.ResultantValue)
+		// }
 
-    return {
-      // RESPONSE
-      // AccessLevels: null,
-      // AdditionalAttributes: _additionalAttributes,
-      // AdditionalWarnings: null,
-      // ConfigurationStatus: "Valid",
-      // CustomModelNumber: uitemplate?.SubtitleTemplate[0]?.Keys[0] + "-N",
-      // Errors: [],
-      // FGID: null, // ! What is FGID??
-      // Features: _features,
-      // GenericModelNumber: uitemplate?.SubtitleTemplate[0]?.Keys[0],
-      // IsConfigured: configure?.IsConfigured ? true : false,
-      // IsFullyConfigured: false,
-      // ModelType: configure?.ModelType,
-      // Number: null,
-      // Product: uitemplate?.ProductIdentifier,
-      // UsedElevatedAccess: false,
-      // Warnings: _warnings,
-
-      // REQUEST
-      ZoomInput: {
-        ShipToNumber: 709323, // Lutron account number
-        LutronSellingCompany: "00101", // Where country exists
-        Product: uitemplate?.ProductIdentifier,
-        Selections: configure?.Selections,
-        AccessLevels: 1, // Place holder (level 1: full access to all selection options)
-      },
-      OverrideSelections: configure?.ResultantValue, // there might be other additionalAttributes other than ResultantValue
-      FeatureDependencies: _featureDependencies,
-      IsQuoted: configure?.IsQuoted,
-    };
-  } else {
-    console.log("[error with UI-template or DPM]");
-  }
-};
+		return {
+			// REQUEST
+			ZoomInput: {
+				ShipToNumber: 709323, // Lutron account number
+				LutronSellingCompany: "00101", // Where country exists
+				Product: uitemplate?.ProductIdentifier,
+				Selections: configure?.Selections,
+				AccessLevels: 1, // Place holder (level 1: full access to all selection options)
+			},
+			OverrideSelections: configure?.ResultantValue, // there might be other additionalAttributes other than ResultantValue
+			FeatureDependencies: _featureDependencies,
+			IsQuoted: configure?.IsQuoted,
+		}
+	} else {
+		console.log("[error with UI-template or DPM]")
+	}
+}
 
 /**
  * This function takes in product and parses the Layout to produce an an array of objects {Variable, RenderType}
  * @param {*} uitemplate == UITempalte
  */
 const parseUITemplate = (uitemplate, UserControlsObj) => {
-  console.log("[parseUITemplate]");
-  // console.log(UserControlsObj)
-  const rows = uitemplate?.Layouts[0]?.Rows;
-  let res = [];
+	console.log("[parseUITemplate]")
+	// console.log(UserControlsObj)
+	const rows = uitemplate?.Layouts[0]?.Rows
+	let res = []
 
-  {
-    rows.map((obj) => {
-      const controls = obj?.Controls[0];
-      const controlVar = controls?.Variable;
+	{
+		rows.map((obj) => {
+			const controls = obj?.Controls[0]
+			const controlVar = controls?.Variable
 
-      res.push({
-        Variable: controlVar,
-        RenderType: componentMapper[UserControlsObj[controlVar].ControlType],
-      });
-    });
-  }
+			res.push({
+				Variable: controlVar,
+				RenderType: componentMapper[UserControlsObj[controlVar].ControlType],
+			})
+		})
+	}
 
-  return res;
-};
+	return res
+}
 
 const convertArrayToObject = (array, key) => {
-  const initialValue = {};
-  return array.reduce((obj, item) => {
-    return {
-      ...obj,
-      [item[key]]: item,
-    };
-  }, initialValue);
-};
+	const initialValue = {}
+	return array.reduce((obj, item) => {
+		return {
+			...obj,
+			[item[key]]: item,
+		}
+	}, initialValue)
+}
+
+/**
+ * handleOnSave takes the user configurations and current zoom values to build out a final object to be passed into lutron's save function
+ * @param {*} zoomReqVal
+ * @param {*} zoomResVal
+ * @param {*} configure The user configuration
+ * @param {*} save_function	The save function passed in my lutron's parent container
+ */
+const handleOnSave = async (zoomReqVal, zoomResVal, configure, save_function) => {
+	console.log("[handleOnSave]")
+}
+
+/**
+ * handleOnCancel handles when the user clicks on cancel
+ */
+const handleOnCancel = async () => {
+	console.log("[handeOnCancel]")
+}
 
 const App = (props) => {
 	// Destructuring props
-	// data will contain the AUTHORIZATION TOKEN
-	const { data, configure } = props
+	// data will contain the AUTHORIZATION TOKEN and a save function
+	const { data, configure, save_function } = props
 	const configure_test = {
 		BillOfMaterialsLineItemId: 7407192,
 		ConfiguredJSON:
@@ -329,35 +340,45 @@ const App = (props) => {
 
 						// Render the DOM
 						return (
-							// Context API, passing the state in
-							<Product.Provider value={{ product, setProduct }}>
-								<ZoomRequest.Provider value={{ zoomReqVal, setZoomReq }}>
-									<ZoomResponse.Provider value={{ zoomResVal, setZoomRes }}>
-										<div className='container'>
-											<div className='row'>
-												<div className='col-1'></div>
-												<div className='col-4'>
-													{/* Only Product Image goes in this div */}
-													<ProductImage product={product}/>
-												</div>
-												<div className='col-6'>
-													{/* Put all other components in this div */}
+							<Application>
+								{/* // Context API, passing the state in */}
+								<Product.Provider value={{ product, setProduct }}>
+									<ZoomRequest.Provider value={{ zoomReqVal, setZoomReq }}>
+										<ZoomResponse.Provider value={{ zoomResVal, setZoomRes }}>
+											<div className='container'>
+												<div className='row'>
+													<div className='col-1'></div>
+													<div className='col-4'>
+														{/* Only Product Image goes in this div */}
+														<ProductImage product={product} />
+													</div>
+													<div className='col-6'>
+														{/* Put all other components in this div */}
 
-													{RenderLayout.map(
-														({ Variable, RenderType }) =>
-															({
-																Icons: <Icons variable={Variable} data={UserControlsObj[Variable]} cdn={product.CDNPrefix} />,
-																SelectionList: <SelectionList variable={Variable} data={UserControlsObj[Variable]} />,
-																// NativeDropdown: <NativeDropdown variable={Variable} data={UserControlsObj[Variable]} cdn={product.CDNPrefix} />,
-															}[RenderType])
-													)}
+														{RenderLayout.map(
+															({ Variable, RenderType }) =>
+																({
+																	Icons: <Icons variable={Variable} data={UserControlsObj[Variable]} cdn={product.CDNPrefix} />,
+																	SelectionList: <SelectionList variable={Variable} data={UserControlsObj[Variable]} />,
+																	// NativeDropdown: <NativeDropdown variable={Variable} data={UserControlsObj[Variable]} cdn={product.CDNPrefix} />,
+																}[RenderType])
+														)}
+													</div>
+													<div className='col-1'></div>
 												</div>
-												<div className='col-1'></div>
 											</div>
-										</div>
-									</ZoomResponse.Provider>
-								</ZoomRequest.Provider>
-							</Product.Provider>
+										</ZoomResponse.Provider>
+									</ZoomRequest.Provider>
+								</Product.Provider>
+								<ApplicationFooter>
+									<button className='button' onClick={(e) => handleOnCancel()}>
+										Cancel
+									</button>
+									<button className='button' onClick={(e) => handleOnSave(zoomResVal, zoomReqVal, configure_test, save_function)}>
+										Save
+									</button>
+								</ApplicationFooter>
+							</Application>
 						)
 					} else {
 						return <div>Awaiting from Configurator API...</div>
