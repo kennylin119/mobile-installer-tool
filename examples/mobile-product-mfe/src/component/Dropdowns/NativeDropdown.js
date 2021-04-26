22a56e394930d248347cc26361f29e356b4537bb
Hannah Fabian <hmf217@lehigh.edu>
2021-04-25 23:14:57 -0400

    Added Zoom API response

--- examples/mobile-product-mfe/src/component/Dropdowns/NativeDropdown.js
+++ examples/mobile-product-mfe/src/component/Dropdowns/NativeDropdown.js
@@ -1,48 +1,28 @@
 import default_img from '../../toolkit/default_image.jpeg';
 import { Product } from "../../product-context";
 import React, { useContext } from "react";
-import { View, Text, Picker, StyleSheet, Image } from 'react-native'
-import { ZoomRequest, ZoomResponse } from "../../zoom-context"
-import ZoomHandler from "../../services/ZoomHandler"
+import { View, Text, Picker, StyleSheet, Image } from 'react-native';
+import { ZoomRequest, ZoomResponse } from "../../zoom-context";
+import ZoomHandler from "../../services/ZoomHandler";
+import NativeDropdownItem from "./NativeDropdownItem";
 
-class NativeDropdown extends React.Component {
-
-   convertArrayToObject = (array, key) => {
-      const initialValue = {}
-      return array.reduce((obj, item) => {
-         return {
-            ...obj,
-            [item[key]]: item,
-         }
-      }, initialValue)
-   }
-
-   state = {selection: ''};
-   updateSelection = (selection) => {
-      this.setState({ selection: selection })
-   }
-
-}
-
-NativeDropdown = (props) => {
+const NativeDropdown = (props) => {
 
    // Using props from Context API
    console.log("[Image-Selection-List]")
-   const { variable, data, cdn } = props
+   const { data, variable } = props
 
    // Setting the Zoom context
    const { zoomReqVal, setZoomReq } = useContext(ZoomRequest)
 	const { zoomResVal, setZoomRes } = useContext(ZoomResponse)
    
-
    // View that is returned
-   // TODO: When a color is selected, only display the coresponding image
    return (
       <div>
          <h3>{data.Variable}</h3>
          <Picker >
          {data.OptionValues.map((option) => {
-            return <Picker.Item label = {option.Label} value = {option.Label} />
+            return <NativeDropdownItem label={option.Label} section={variable} keyValue = {option.keyValue} />;
          })}
          </Picker>
 		</div>
---
+++ examples/mobile-product-mfe/src/component/Dropdowns/NativeDropdownItem.js
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import { Picker } from 'react-native';
+import { ZoomRequest, ZoomResponse } from "../../zoom-context";
+import ZoomHandler from "../../services/ZoomHandler";
+
+// Zoom response
+const handleOnClick = async (event, keyValue, section, zoomReqVal, zoomResVal, setZoomReq, setZoomRes) => {
+   console.log("[Handle Image-SelectionButton click]");
+ 
+   // prevents broswer refresh
+   event.preventDefault();
+   if (zoomReqVal.ZoomInput.Selections[section] !== keyValue) {
+     await setZoomReq((prevState) => {
+       prevState.ZoomInput.Selections[section] = keyValue;
+   
+       return prevState;
+     })
+ 
+     let _response = await ZoomHandler(zoomReqVal);
+     setZoomRes(_response);
+   }
+ };
+
+const NativeDropdownItem = (props) => {
+   
+   // Setting the props and Zoom res
+   const { label, section, keyValue } = props;
+   const { zoomReqVal, setZoomReq } = useContext(ZoomRequest);
+   const { zoomResVal, setZoomRes } = useContext(ZoomResponse);
+
+   // Returning the picker item
+   return (
+      <Picker.Item label = {label} value = {label} onClick={(e) => handleOnClick(e, keyValue, section, zoomReqVal, zoomResVal, setZoomReq, setZoomRes)}> {label} </Picker.Item>
+   )
+}
+
+export default NativeDropdownItem;
+
