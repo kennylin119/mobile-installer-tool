7739eb5c48d5af0d1ee3a26562ad2289ed1feb16
Hannah Fabian <hmfabian1@gmail.com>
2021-09-21 16:07:52 -0400

    Created an image placeholder

---
+++ examples/mobile-product-mfe/src/main/component/Placeholders/ImagePlaceholder.js
@@ -0,0 +1,11 @@
+import { Placeholder } from 'semantic-ui-react';
+
+const ImagePlaceholder = () => {
+  return (
+    <Placeholder style={{ height: 150, width: 150 }}>
+        <Placeholder.Image />
+    </Placeholder>
+  );
+};
+
+export default ImagePlaceholder;
