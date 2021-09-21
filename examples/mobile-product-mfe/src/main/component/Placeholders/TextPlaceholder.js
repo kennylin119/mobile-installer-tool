d14bad0e810ce1dd8ca44b778f02a0849d00022f
Hannah Fabian <hmfabian1@gmail.com>
2021-09-21 16:03:49 -0400

    Created text placeholder (general)

---
+++ examples/mobile-product-mfe/src/main/component/Placeholders/TextPlaceholder.js
@@ -0,0 +1,12 @@
+import { Placeholder } from 'semantic-ui-react';
+
+const TextPlaceholder = () => {
+  return (
+    <Placeholder>
+      <Placeholder.Line length='full' />
+      <Placeholder.Line length='full' />
+    </Placeholder>
+  );
+};
+
+export default TextPlaceholder;
