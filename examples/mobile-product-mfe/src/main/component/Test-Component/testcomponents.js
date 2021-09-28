701ef268e1b060cbc128dba1684fdd0a9ba1da34
Alan Wang <alanwang53@gmail.com>
2021-09-28 15:39:13 -0400

    created empty test files in __test__

--- examples/mobile-product-mfe/src/main/component/Test-Component/testcomponents.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const testcomponents = () => {
-  console.log('in test component');
-
-  return <div>testcomponent asdfsadfsadfsadfsdf</div>;
-};
-
-export default testcomponents;
